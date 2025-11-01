3946cdc737382d61da676de3f56e2601c69f291e
lamtanphat2202 <tphatt22@gmail.com>
2025-11-02 00:03:29 +0700

    config template engine using handlebar

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const { engine } = require("express-handlebars");
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
+
+//Http logger middleware
+app.use(morgan("combined"));
+
+// Handlebars setup
+app.engine(
+    "hbs",
+    engine({
+        extname: ".hbs",
+    })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/news", (req, res) => {
+    res.render("news");
+});
+app.listen(port, () => console.log("test port"));
