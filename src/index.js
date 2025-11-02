af3cd636ef63543492fae7ab6b0eb781e76617cd
lamtanphat2202 <tphatt22@gmail.com>
2025-11-03 01:30:56 +0700

    config sass

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+// Để phục vụ các file tĩnh như css, js, hình ảnh,...
+// Nếu không có dòng này thì khi truy cập vào các file tĩnh sẽ bị lỗi 404 not found
+app.use(express.static(path.join(__dirname, "public")));
+
 //Http logger middleware
 app.use(morgan("combined"));
 
@@ -16,6 +20,9 @@ app.engine(
     })
 );
 app.set("view engine", "hbs");
+// Dòng bên dưới để set đường dẫn đến thư mục views để thằng express biết nó nằm đâu
+// Nếu file index.js + folder views nằm cùng cấp với node_modules thì không cần dòng này
+// Nhưng nếu không cùng cấp với node_moldules thì phải set đường dẫn cho nó.
 app.set("views", path.join(__dirname, "resources/views"));
 
 app.get("/", (req, res) => {
